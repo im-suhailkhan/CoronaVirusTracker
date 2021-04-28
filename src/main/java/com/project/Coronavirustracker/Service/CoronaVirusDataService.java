6cc29f06ca45924ef6665a9f556d0eec31d21ea0
suhailkhanqbox <suhail.khan.ftg@gmail.com>
2021-04-28 14:08:00 +0530

    prased csv using apache opensource

--- src/main/java/com/project/Coronavirustracker/Service/CoronaVirusDataService.java
+++ src/main/java/com/project/Coronavirustracker/Service/CoronaVirusDataService.java
@@ -1,9 +1,12 @@
 package com.project.Coronavirustracker.Service;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -22,6 +25,13 @@ public class CoronaVirusDataService {
                 .build();
         HttpResponse<String> httpResponse=httpClient.send(request,HttpResponse.BodyHandlers.ofString());
         System.out.println(httpResponse.body());
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for (CSVRecord record : records) {
+            String state=record.get("Province/State");
+            System.out.println(state);
+        }
+
 
     }
 }
